c00b8285efce0aa3f7babcef8f2adeeacc4b9ade
BaeJihyun97 <baejihyun970519@gmail.com>
2024-04-07 22:36:04 +0900

    [level 1] Title: K번째수, Time: 0.35 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for(int i=0; i < commands.length; i++) {
+            int[] command = commands[i];
+            int[] temp = Arrays.copyOfRange(array, command[0]-1, command[1]);
+            Arrays.sort(temp);
+            answer[i] = temp[command[2]-1];
+
+        }
+        
+        return answer;
+    }
+}
