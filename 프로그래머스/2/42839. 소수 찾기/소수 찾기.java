e4ee4a2d2947eb4ce2d790902eb94fef3c406437
BaeJihyun97 <baejihyun970519@gmail.com>
2024-04-09 22:41:37 +0900

    [level 2] Title: 소수 찾기, Time: 1.40 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,39 +1,60 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 
 class Solution {
     
-    ArrayList<Integer> primes = new ArrayList<>();
-    
-    void findPrimes(int maxV) {
-        for (int i=2; i <= maxV; i++) {
-            primes.add(i);
+    boolean check(String num) {
+        int number = Integer.parseInt(num);
+        int numMax = (int) Math.sqrt(number);
+        
+        if (number < 2) {
+            return false;
         }
         
-        for (int i=2; i <= maxV; i++) {
-            if (primes.contains(i)) {
-                for (int j=i*2; j<= maxV; j+=i) {
-                    if (primes.indexOf(j) >= 0) {
-                        primes.remove(primes.indexOf(j));
-                    }
-                }
+        for (int i=2; i <= numMax; i++) {
+            if (number % i == 0) {
+                return false;
             }
         }
+        return true;
+    }
+    
+    void dfs(char[] numberArray, HashSet<Integer> set, boolean[] visited, int depth, int r, StringBuffer curr) {
+        if (depth == r) {
+            return;
+        }
+        
+        for (int i=0;i<numberArray.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                curr.append(numberArray[i]);
+                
+                if (check(curr.toString())) {
+                    set.add(Integer.parseInt(curr.toString()));
+                }
+
+                dfs(numberArray, set, visited, depth+1, r, curr);
+                
+                curr.deleteCharAt(curr.length() - 1);
+                visited[i] = false;
+            }
+        }
+        return;
     }
     
     public int solution(String numbers) {
         int answer = 0;
-        int [] numberArray = numbers.chars().map(i->i-'0').toArray();
+        char [] numberArray = numbers.toCharArray();
+        HashSet<Integer> set = new HashSet<>();
+        boolean[] visited = new boolean[numberArray.length];
+        
+        Arrays.fill(visited, false);
         Arrays.sort(numberArray);
         
-        int maxNum = 0;
-        for(int i=numberArray.length-1; i >= 0 ; i--) {
-            maxNum *= 10;
-            maxNum += numberArray[i];
-        }
+        dfs(numberArray, set, visited, 0, numberArray.length, new StringBuffer());
         
-        findPrimes(maxNum);
-        
+        answer = set.size();
         return answer;
     }
 }
