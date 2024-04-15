7aeecb54cb51b1a540ec03888d527ceac9e8f925
BaeJihyun97 <baejihyun970519@gmail.com>
2024-04-15 23:05:00 +0900

    [level 1] Title: 체육복, Time: 3.67 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,48 @@
+import java.util.HashSet;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        boolean[] exist = new boolean[n];
+        HashSet<Integer> lostSet = new HashSet<>(Arrays.stream(lost).boxed().collect(Collectors.toList()));
+        HashSet<Integer> reserveSet = new HashSet<>(Arrays.stream(reserve).boxed().collect(Collectors.toList()));
+        
+        // 본인 체육복 먼저 챙기기
+        for (int i=1; i <= n; i++) {
+            if (lostSet.contains(i)) {
+                if(reserveSet.contains(i)) {
+                    exist[i-1] = true;
+                    reserveSet.remove(i);
+                    lostSet.remove(i);
+                }
+                else {
+                    exist[i-1] = false;
+                }
+            }
+            else {
+                exist[i-1] = true;
+            }
+        }
+        
+        int[] temp = lostSet.stream().mapToInt(Integer::intValue).toArray();
+        Arrays.sort(temp);
+        answer = temp.length;
+        
+        // 빌려주기
+        for (int l: temp) {
+            if (reserveSet.contains(l-1)) {
+                answer -= 1;
+                reserveSet.remove(l-1);
+            }
+            else if (reserveSet.contains(l+1)) {
+                answer -= 1;
+                reserveSet.remove(l+1);
+            }
+        }
+        
+        
+        return n - Math.max(answer, 0);
+    }
+}
