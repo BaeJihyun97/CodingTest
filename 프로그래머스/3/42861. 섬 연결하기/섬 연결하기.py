016350be81c2686a6bbb0fc31d5bb7762b1c3be5
BaeJihyun97 <baejihyun970519@gmail.com>
2024-04-24 23:31:04 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,3 +1,5 @@
+# union & find 알고리즘
+
 def find(v, union):
     if union[v] != v:
         union[v] = find(union[v], union)
@@ -18,6 +20,7 @@ def solution(n, costs):
             count += 1
             answer += cost
             
+            # 주의! 최종 parent의 값을 바꿔야함. n1, n2가 아님!!!!
             value = min(f1, f2)
             union[f1] = value
             union[f2] = value
