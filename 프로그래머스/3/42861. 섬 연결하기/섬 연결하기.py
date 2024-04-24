0fb25d784575d0401a2ecedf43761d5fcedb8cad
BaeJihyun97 <baejihyun970519@gmail.com>
2024-04-24 23:29:22 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -0,0 +1,27 @@
+def find(v, union):
+    if union[v] != v:
+        union[v] = find(union[v], union)
+    return union[v]
+    
+
+def solution(n, costs):
+    answer = 0
+    costs.sort(key = lambda x: x[2])
+    union = [i for i in range(n)]
+    count = 0
+    
+    while count < n-1 and len(costs) > 0:
+        [n1, n2, cost] = costs.pop(0)
+        f1, f2 = find(n1, union), find(n2, union)
+        if f1 != f2:
+            
+            count += 1
+            answer += cost
+            
+            value = min(f1, f2)
+            union[f1] = value
+            union[f2] = value
+            
+            # print(union)
+            
+    return answer
