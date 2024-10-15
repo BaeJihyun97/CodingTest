d47b24c4a4f42f8af2864e3fdc3e1186213db32a
BaeJihyun97 <baejihyun970519@gmail.com>
2024-10-15 18:58:42 +0900

    [level 3] Title: 합승 택시 요금, Time: 2452.68 ms, Memory: 13 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,5 +1,12 @@
+# 알고리즘: 딕스트라 3번(O(VlogE)) 또는 플로이드 워셜(O(V^3)
+# 자료구조: heaqque
+# 주의: 문제 풀이를 좀더 알고리즘화 시켜서 생각하기
+#       s -> i, i -> a, i -> b 의 경로 합 가중치가 최소인것.
+#       s -> i, a -> i, b -> i 각각의 최소값 찾기
+#       s, a, b 딕스트라 3번 또는 플로이드 워셜
 import heapq
 from collections import deque
+
 
 MAXV = 100001*200
 
@@ -26,23 +33,49 @@ def dijkstra(n, graph, node):
                 
     return dp
 
+def FloydWarshall(n, s, a, b, fares):
+    graph = [[MAXV]*(n+1) for _ in range(n+1)]
+    
+    for n1, n2, w in fares:
+        graph[n1][n2] = w
+        graph[n2][n1] = w
+        
+    for i in range(1, n+1):
+        graph[i][i] = 0
+    
+    for k in range(1, n+1):
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+    
+    answer = graph[s][a] + graph[s][b]        
+    for i in range(1, n+1):
+        new_fare = graph[s][i] + graph[i][a] + graph[i][b]
+        if answer > new_fare:
+            answer = new_fare
+    return answer
+
 def solution(n, s, a, b, fares):
     answer = 0
     
-    # make graph
-    graph = [[] for _ in range(n+1)]
-    for n1, n2, w in fares:
-        graph[n1].append((n2, w))
-        graph[n2].append((n1, w))
+    # dijkstra
+#     graph = [[] for _ in range(n+1)]
+#     for n1, n2, w in fares:
+#         graph[n1].append((n2, w))
+#         graph[n2].append((n1, w))
     
-    a_path = dijkstra(n, graph, a)
-    b_path = dijkstra(n, graph, b)
-    s_path = dijkstra(n, graph, s)
+#     a_path = dijkstra(n, graph, a)
+#     b_path = dijkstra(n, graph, b)
+#     s_path = dijkstra(n, graph, s)
     
-    answer = MAXV
-    for i in range(1, n+1):
-        new_fare = s_path[i] + a_path[i] + b_path[i]
-        if answer > new_fare:
-            answer = new_fare
+#     answer = MAXV
+#     for i in range(1, n+1):
+#         new_fare = s_path[i] + a_path[i] + b_path[i]
+#         if answer > new_fare:
+#             answer = new_fare
+
+    # floyd-warshall
+    answer = FloydWarshall(n, s, a, b, fares)
 
     return answer
